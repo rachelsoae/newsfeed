fb996c20d16cf8a2b68f5729c49f82ecce06aad7
Rachel Prather <rachelprather1@gmail.com>
2023-09-18 10:34:09 -0700

    Create component subdirectories

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import Nav from '../Nav/Nav';
+
+const App = () => {
+  return (
+    <>
+      <Nav />
+      <main>
+        {/* search */}
+        {/* cards */}
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,9 @@
+const Nav = () => {
+  return (
+    <div className='Nav'>
+      <h1>NEWSFEED</h1>
+    </div>
+  )
+}
+
+export default Nav;
