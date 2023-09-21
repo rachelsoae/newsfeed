c2851f6c3eaa989d6387bf051b1c6b06317d8f60
Rachel Prather <rachelprather1@gmail.com>
2023-09-21 10:30:51 -0700

    Create and style article cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,16 @@
 const Card = () => {
   return (
-    <h3>Card</h3>
+    <article className='Card'>
+      <img className='Card__img' src='https://cdn.cnn.com/cnnnext/dam/assets/230916152026-02-uaw-autoworkers-strike-0916-super-tease.jpg'/>
+      <span className='Card__text'>
+        <h2 className='Card__title'>United Auto Workers strike enters third day: Live updates - CNN</h2>
+        <p>The United Auto Workers' strike against General Motors, Ford and Stellantis has entered its third day. Follow here for the latest.</p>
+        <div className='Card__details'>
+          <p>09/17/23</p>
+          <p>CNN</p>
+        </div>
+      </span>
+    </article>
   )
 }
 
