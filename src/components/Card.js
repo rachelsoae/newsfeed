815da8ea54cb7a9fa22b8b8afb0e0475ca1fd786
Rachel Prather <rachelprather1@gmail.com>
2023-09-21 11:43:03 -0700

    Format publication date

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,20 @@
 import { Link } from 'react-router-dom';
 
 const Card = ({article}) => {
-  console.log(article)
+  const formatDate = dateString => {
+    const date = new Date(dateString).toString();
+    return date.slice(0, 21);
+  }
+
   return (
-    <Link to={`/12`}>
+    <Link to={`/12`} id={article.publishedAt}>
       <article className='Card'>
         <img className='Card__img' src={article.urlToImage}/>
         <span className='Card__text'>
           <h2 className='Card__title'>{article.title}</h2>
           <p>{article.description}</p>
           <div className='Card__details'>
-            <p>{article.publishedAt}</p>
+            <p>{formatDate(article.publishedAt)}</p>
             <p>{article.source.name}</p>
           </div>
         </span>
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 import Card from './Card';
 
 const Home = ({articles}) => {
-  const cards = articles.map(article => <Card article={article} />)
+  const cards = articles.map(article => <Card key={article.publishedAt} article={article} />)
   
   return (
     <main className='Home'>
