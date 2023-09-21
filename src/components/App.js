8545c098feb02417e219a11eb74e3555d12fd7bc
Rachel Prather <rachelprather1@gmail.com>
2023-09-21 09:24:36 -0700

    Set up component files and routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,7 @@
+import { Routes, Route } from 'react-router-dom';
 import Nav from './Nav';
+import ArticleDetail from './ArticleDetail';
+import Card from './Card';
 
 const App = () => {
   return (
@@ -8,6 +11,10 @@ const App = () => {
         {/* search */}
         {/* cards */}
       </main>
+      <Routes>
+        <Route path='/:id' element={<ArticleDetail />}/>
+        <Route path='/error' element={<Error />}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/ArticleDetail.js
@@ -0,0 +1,7 @@
+const ArticleDetail = () => {
+  return (
+    <h2>article detail</h2>
+  )
+}
+
+export default ArticleDetail;
